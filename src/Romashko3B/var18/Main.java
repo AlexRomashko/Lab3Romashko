03ad6248a29c78ab60cdde156a70e7b98881c521
AlexRomashko <92051492+AlexRomashko@users.noreply.github.com>
2021-12-13 00:40:50 +0300

    add reference panel

--- src/Romashko3B/var18/Main.java
+++ src/Romashko3B/var18/Main.java
@@ -47,6 +47,7 @@ public class Main extends JFrame {
     private JMenuItem saveToTextMenuItem;
     private JMenuItem saveToGraphicsMenuItem;
     private JMenuItem searchValueMenuItem;
+    private JMenuItem reference;
 
     // Поля ввода для считывания значений переменных
     private JTextField textFieldFrom;
@@ -75,41 +76,26 @@ public class Main extends JFrame {
 // Отцентрировать окно приложения на экране
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
-
 // Создать меню
         JMenuBar menuBar = new JMenuBar();
 
-// Установить меню в качестве главного меню приложения
+//добавляем пункты в меню
         setJMenuBar(menuBar);
-
-// Добавить в меню пункт меню "Файл"
-
         JMenu fileMenu = new JMenu("Файл");
-
-// Добавить его в главное меню
         menuBar.add(fileMenu);
-
-// Создать пункт меню "Таблица"
-
         JMenu tableMenu = new JMenu("Таблица");
-
-// Добавить его в главное меню
         menuBar.add(tableMenu);
+        JMenu MenuReference = new JMenu("Справка");
+        menuBar.add(MenuReference);
 
-// Создать новое "действие" по сохранению в текстовый файл
+// Создаём новое "действие" по сохранению в текстовый файл
         Action saveToTextAction = new AbstractAction("Сохранить в текстовый файл") {
             public void actionPerformed(ActionEvent event) {
                 if (fileChooser==null) {
-// Если экземпляр диалогового окна "Открыть файл" ещѐ не создан,
-// то создать его
                     fileChooser = new JFileChooser();
-// и инициализировать текущей директорией
                     fileChooser.setCurrentDirectory(new File("."));
                 }
-// Показать диалоговое окно
                 if (fileChooser.showSaveDialog(Main.this) == JFileChooser.APPROVE_OPTION)
-// Если результат его показа успешный,
-// сохранить данные в текстовый файл
                     saveToTextFile(fileChooser.getSelectedFile());
             }
         };
@@ -121,105 +107,79 @@ public class Main extends JFrame {
         Action saveToGraphicsAction = new AbstractAction("Сохранить данные для построения графика") {
             public void actionPerformed(ActionEvent event) {
                 if (fileChooser==null) {
-// Если экземпляр диалогового окна
-// "Открыть файл" ещѐ не создан,
-// то создать его
                     fileChooser = new JFileChooser();
-// и инициализировать текущей директорией
                     fileChooser.setCurrentDirectory(new File("."));
                 }
-// Показать диалоговое окно
-                if (fileChooser.showSaveDialog(Main.this) ==
-                        JFileChooser.APPROVE_OPTION);
-// Если результат его показа успешный,
-// сохранить данные в двоичный файл
-                saveToGraphicsFile(
-                        fileChooser.getSelectedFile());
+                if (fileChooser.showSaveDialog(Main.this) == JFileChooser.APPROVE_OPTION);
+                saveToGraphicsFile(fileChooser.getSelectedFile());
             }
         };
 // Добавить соответствующий пункт подменю в меню "Файл"
         saveToGraphicsMenuItem = fileMenu.add(saveToGraphicsAction);
-// По умолчанию пункт меню является недоступным(данных ещѐ нет)
         saveToGraphicsMenuItem.setEnabled(false);
+
 // Создать новое действие по поиску значений многочлена
         Action searchValueAction = new AbstractAction("Найти значение многочлена") {
             public void actionPerformed(ActionEvent event) {
-// Запросить пользователя ввести искомую строку
-                String value =
-                        JOptionPane.showInputDialog(Main.this, "Введите значение для поиска",
-                                "Поиск значения", JOptionPane.QUESTION_MESSAGE);
+                String value = JOptionPane.showInputDialog(Main.this, "Введите значение для поиска", "Поиск значения", JOptionPane.QUESTION_MESSAGE);
 // Установить введенное значение в качестве иголки
                 renderer.setNeedle(value);
 // Обновить таблицу
                 getContentPane().repaint();
             }
         };
+
 // Добавить действие в меню "Таблица"
         searchValueMenuItem = tableMenu.add(searchValueAction);
-// По умолчанию пункт меню является недоступным (данных ещѐ нет)
         searchValueMenuItem.setEnabled(false);
+
+        Action ReferenceAction = new AbstractAction("О программе") {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(Main.this, "ЭТО СПРАВКА!!!");
+            }
+        };
+        reference = MenuReference.add(ReferenceAction);
+        reference.setEnabled(true);
+
 // Создать область с полями ввода для границ отрезка и шага
 // Создать подпись для ввода левой границы отрезка
         JLabel labelForFrom = new JLabel("X изменяется на интервале от:");
-// Создать текстовое поле для ввода значения длиной в 10 символов
-// со значением по умолчанию 0.0
         textFieldFrom = new JTextField("0.0", 10);
-// Установить максимальный размер равный предпочтительному, чтобы
-// предотвратить увеличение размера поля ввода
         textFieldFrom.setMaximumSize(textFieldFrom.getPreferredSize());
-// Создать подпись для ввода левой границы отрезка
         JLabel labelForTo = new JLabel("до:");
-// Создать текстовое поле для ввода значения длиной в 10 символов
-// со значением по умолчанию 1.0
         textFieldTo = new JTextField("1.0", 10);
-// Установить максимальный размер равный предпочтительному, чтобы
-// предотвратить увеличение размера поля ввода
         textFieldTo.setMaximumSize(textFieldTo.getPreferredSize());
-// Создать подпись для ввода шага табулирования
         JLabel labelForStep = new JLabel("с шагом:");
-// Создать текстовое поле для ввода значения длиной в 10 символов
-// со значением по умолчанию 1.0
         textFieldStep = new JTextField("0.1", 10);
-// Установить максимальный размер равный предпочтительному, чтобы
-// предотвратить увеличение размера поля ввода
+
+// Установить максимальный размер равный предпочтительному, чтобы предотвратить увеличение размера поля ввода
         textFieldStep.setMaximumSize(textFieldStep.getPreferredSize());
 // Создать контейнер 1 типа "коробка с горизонтальной укладкой"
         Box hboxRange = Box.createHorizontalBox();
 // Задать для контейнера тип рамки "объѐмная"
         hboxRange.setBorder(BorderFactory.createBevelBorder(1));
-// Добавить "клей" C1-H1
         hboxRange.add(Box.createHorizontalGlue());
-// Добавить подпись "От"
         hboxRange.add(labelForFrom);
-// Добавить "распорку" C1-H2
         hboxRange.add(Box.createHorizontalStrut(10));
-// Добавить поле ввода "От"
         hboxRange.add(textFieldFrom);
-// Добавить "распорку" C1-H3
         hboxRange.add(Box.createHorizontalStrut(20));
-// Добавить подпись "До"
         hboxRange.add(labelForTo);
-// Добавить "распорку" C1-H4
         hboxRange.add(Box.createHorizontalStrut(10));
-// Добавить поле ввода "До"
         hboxRange.add(textFieldTo);
-// Добавить "распорку" C1-H5
         hboxRange.add(Box.createHorizontalStrut(20));
-// Добавить подпись "с шагом"
         hboxRange.add(labelForStep);
-// Добавить "распорку" C1-H6
         hboxRange.add(Box.createHorizontalStrut(10));
-// Добавить поле для ввода шага табулирования
         hboxRange.add(textFieldStep);
-// Добавить "клей" C1-H7
         hboxRange.add(Box.createHorizontalGlue());
-// Установить предпочтительный размер области равным удвоенному
-// минимальному, чтобы при компоновке область совсем не сдавили
+
+// Установить предпочтительный размер области равным удвоенному минимальному, чтобы при компоновке область совсем не сдавили
         hboxRange.setPreferredSize(new Dimension(
                 new Double(hboxRange.getMaximumSize().getWidth()).intValue(),
                 new Double(hboxRange.getMinimumSize().getHeight()).intValue()*2));
 // Установить область в верхнюю (северную) часть компоновки
         getContentPane().add(hboxRange, BorderLayout.NORTH);
+
+
 // Создать кнопку "Вычислить"
         JButton buttonCalc = new JButton("Вычислить");
 // Задать действие на нажатие "Вычислить" и привязать к кнопке
@@ -227,12 +187,9 @@ public class Main extends JFrame {
             public void actionPerformed(ActionEvent ev) {
                 try {
 // Считать значения начала и конца отрезка, шага
-                    Double from =
-                            Double.parseDouble(textFieldFrom.getText());
-                    Double to =
-                            Double.parseDouble(textFieldTo.getText());
-                    Double step =
-                            Double.parseDouble(textFieldStep.getText());
+                    Double from = Double.parseDouble(textFieldFrom.getText());
+                    Double to = Double.parseDouble(textFieldTo.getText());
+                    Double step = Double.parseDouble(textFieldStep.getText());
 // На основе считанных данных создать новый экземпляр модели таблицы
                     data = new GornerTableModel(from, to, step, Main.this.coefficients);
 // Создать новый экземпляр таблицы
@@ -301,6 +258,7 @@ public class Main extends JFrame {
 // Установить контейнер hBoxResult в главной (центральной) области граничной компоновки
         getContentPane().add(hBoxResult, BorderLayout.CENTER);
     }
+
     protected void saveToGraphicsFile(File selectedFile) {
         try {
 // Создать новый байтовый поток вывода, направленный в указанный файл
@@ -314,8 +272,6 @@ public class Main extends JFrame {
 // Закрыть поток вывода
             out.close();
         } catch (Exception e) {
-// Исключительную ситуацию "ФайлНеНайден" в данном случае можно не обрабатывать,
-// так как мы файл создаѐм, а не открываем для чтения
         }
     }
     protected void saveToTextFile(File selectedFile) {
@@ -343,35 +299,27 @@ public class Main extends JFrame {
 // Закрыть поток
             out.close();
         } catch (FileNotFoundException e) {
-// Исключительную ситуацию "ФайлНеНайден" можно не
-// обрабатывать, так как мы файл создаѐм, а не открываем
         }
     }
     public static void main(String[] args) {
-// Если не задано ни одного аргумента командной строки -
-// Продолжать вычисления невозможно, коэффиценты неизвестны
+
         if (args.length==0) {
             System.out.println("Невозможно табулировать многочлен, для которого не задано ни одного коэффициента!");
             System.exit(-1);
         }
-// Зарезервировать места в массиве коэффициентов столько, сколько аргументов командной строки
         Double[] coefficients = new Double[args.length];
         int i = 0;
         try {
-// Перебрать аргументы, пытаясь преобразовать их в Double
             for (String arg: args) {
                 coefficients[i++] = Double.parseDouble(arg);
             }
         }
         catch (NumberFormatException ex) {
-// Если преобразование невозможно - сообщить об ошибке и завершиться
             System.out.println("Ошибка преобразования строки '" +
                     args[i] + "' в число типа Double");
             System.exit(-2);
         }
-// Создать экземпляр главного окна, передав ему коэффициенты
         Main frame = new Main(coefficients);
-// Задать действие, выполняемое при закрытии окна
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
